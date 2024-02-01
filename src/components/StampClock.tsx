072ebf217fa0a62c6b55010e96d4bfc8a39cf533
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-02-01 15:26:31 +0100

    Added in or out

--- src/components/StampClock.tsx
+++ src/components/StampClock.tsx
@@ -1,14 +1,39 @@
 'use client';
 
 
+import {useState} from "react";
+
 const StampClock = () => {
+
+    const [inn, setInn] = useState(false);
+
+    const setInnTrueHandler = () => {
+        setInn(true);
+    }
+
+    const setInnFalseHandler = () => {
+        setInn(false);
+    }
+
+
     return (
+
         <div className="flex flex-col items-center justify-center">
-            <button className="bg-green-500 hover:bg-green-700 text-white text-xl font-bold py-3 px-12   rounded">
-                Inn
+            {!inn &&
+                <button className="bg-green-500 hover:bg-green-700 text-white text-xl font-bold py-3 px-12  rounded"
+                        onClick={setInnTrueHandler}>
+                    In
+                </button>
+            }
+            {inn && <button className="bg-red-500 hover:bg-red-700 text-white text-xl font-bold py-3 px-12  rounded"
+                            onClick={setInnFalseHandler}>
+                Out
             </button>
+            }
+
         </div>
-    );
+    )
+        ;
 };
 
 export default StampClock;
